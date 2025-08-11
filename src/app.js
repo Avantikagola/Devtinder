874fa7bb27b8fdfb090f76d1f48256a11adfddba
Avantikagola <avantika.gola05@gmail.com>
2025-08-11 13:47:20 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,16 +2,22 @@ const express = require('express'); // require our express from node module
 
 const app = express(); //creating a new application of express
 
+// this will only handle GET call to /user
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Avantika", lastName: "Gola" });
+});
+
+app.post("/user", (req, res) => {
+    // saving data to DB
+    res.send("Data successfully saved to the database!");
+});
+
+app.delete("/user", (req,res)=>{
+    res.send("deleted successfully");
+});
+// this will match all the HTTP method API call to /test 
 app.use("/test",(req,res)=>{
    res.send("hello hello ");
-});
-
-app.use("/hello",(req,res)=>{
-   res.send("hello");
-});
-
-app.use("/",(req,res)=>{
-   res.send("hello hello hello");
 });
 
 app.listen(7777,()=>{
