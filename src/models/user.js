d935dae72ee423255cdbeeb850585741040e52bd
Avantikagola <avantika.gola05@gmail.com>
2025-08-22 13:17:30 +0530

    schema validation

--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,52 @@ const mongoose= require('mongoose');
 
 const userSchema= new mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
     },
     lastName:{
         type:String
     },
     id:{
-        type:String
+        type:String,
+        required:true,
+        unique: true,
+        lowercase:true,
+        trim:true,
     },
     password:{
-        type:String
+        type:String,
+        required:true,
     },
     age:{
-        type:Number
+        type:Number,
+        min:18,
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!["male","female","other"].includes(value)){
+                throw new error("Gender is not valid");
+            }
+        },
     },
-});
+    photUrl:{
+        type:string
+    },
+    about:{
+        type:string,
+        default:"This is the default about of array",
+    },
+    skills:{
+        type:[string],
+    },
+    createdAt:{
+        type:Date,
+    }
+},
+{
+    timestamps:true,
+}
+);
 
 module.exports=mongoose.model("User",userSchema);
